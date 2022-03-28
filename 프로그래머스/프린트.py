a9e11085175b1a659e0b331a5c0bb30349beb67b
mingyu Lee <dstjr2434@gmail.com>
2022-03-28 18:29:29 +0900

    ahdm

---
+++ 프로그래머스/프린트.py
@@ -0,0 +1,10 @@
+def solution(priorities, location):
+    
+    for idx, num in enumerate(priorities) :
+        
+        if priorities[num]>priorities[num+1]:
+            
+        # print(num)
+    answer = 0
+    return answer
+print(solution([2, 1, 3, 2],2))
